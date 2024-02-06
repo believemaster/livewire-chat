cbe453731d0b613b9ea788f8ef2118c1acaa015b
believemaster <believemasters@gmail.com>
2024-02-06 16:01:23 +0530

    Some css changes chat list

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,7 +1,12 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <div class="chat_list_header">
-
+        <div class="title">
+            Chat
+        </div>
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="" srcset="">
+        </div>
     </div>
 
     <div class="chat_list_body">
