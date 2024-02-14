246b04c31517ba70579b492b8927556cc2c8f1d8
believemaster <believemasters@gmail.com>
2024-02-14 16:32:52 +0530

    Chat list styling done pt2

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -10,6 +10,25 @@
     </div>
 
     <div class="chat_list_body">
-        
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
+                <img src="https://picsum.photos/id/200/200/300" alt="" srcset="">
+            </div>
+
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">{{ Auth::user()->name }}</div>
+                    <span class="date">2 Days ago</span>
+                </div>
+                <div class="bottom_row">
+                    <div class="message_body text-truncate">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quam illo atque, ex ipsam
+                        soluta. Beatae saepe iste, architecto alias, sapiente cum quo laborum obcaecati corporis fuga
+                        soluta minus voluptas!
+                    </div>
+                    <div class="unread_count">56</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
